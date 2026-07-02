51c0846ae276182ec6af9c9d57fb38348af847e4
Wasdawad75 <beeren1907@gmail.com>
2026-07-01 23:32:32 -0500

    Hello There

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+//every task should have the following
+/*
+id
+description
+status | to do , in progress, done
+createdAt
+updatedAt
 
+
+We can have a start page where input can be given, either see tasks, completed/filtered tasks, or add tasks,
+*/
+
+/*
+All tasks should be stored in a json file
+*/
+
+//First lets just make an input
+int main(){
+std::string info;
+std::cout << "Write Something";
+std::cin>> info;
+return 0;
+}
